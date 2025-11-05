29696b5c2cc35d186e303dfdb7334102d118dc24
Cerasela-Nicola Soare <ceraselanicola@yahoo.com>
2025-11-05 15:19:05 +0200

    Added TryDec, TryDate.

--- src/BudgetBuddy.Domain/Extensions.cs
+++ src/BudgetBuddy.Domain/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.IO.Pipelines;
 
 namespace BudgetBuddy.Domain;
 
@@ -26,4 +27,32 @@ public static class Extensions
         ArgumentNullException.ThrowIfNull(arr);
         return arr.Average(x => Math.Abs(x));
     }
+
+    public static string MonthKey(this DateTime date)
+    {
+        return date.ToString("yyyy-MM", CultureInfo.InvariantCulture);
+    }
+
+    public static Result<decimal> TryDec(this string text)
+    {
+        if (decimal.TryParse(text, NumberStyles.Number, CultureInfo.InvariantCulture, out var value))
+        {
+            return Result<decimal>.Ok(value);
+        }
+        else
+        {
+            return Result<decimal>.Fail($"Invalid decimal: '{text}'");
+        }
+    } 
+    
+        public static Result<DateTime> TryDate(this string text)
+    {
+        if(DateTime.TryParseExact(text,"yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out var date))
+        {
+            return Result<DateTime>.Ok(value);
+        } else
+        {
+            return Result<DateTime>.Fail($"Invalid date: '{text}'");
+        }
+    } 
 }
