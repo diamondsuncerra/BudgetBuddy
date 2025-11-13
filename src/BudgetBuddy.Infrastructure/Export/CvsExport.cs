dab1c0aae4354df5d91fb4fcdfa50400830dff14
Cerasela-Nicola Soare <ceraselanicola@yahoo.com>
2025-11-13 18:00:35 +0200

    Last commit of the day.

--- src/BudgetBuddy.Infrastructure/Export/CvsExport.cs
+++ src/BudgetBuddy.Infrastructure/Export/CvsExport.cs
@@ -9,12 +9,6 @@ namespace BudgetBuddy.Infrastructure.Export
     public class CvsExport : IExportStrategy
     {
 
-
-        public static void Export(string fileName, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
         public static Task Export(string fileName, IDictionary<string, Transaction> data, CancellationToken token)
         {
             throw new NotImplementedException();
