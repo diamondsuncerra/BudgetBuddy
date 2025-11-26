30d70daee8329c42da3fdab4cba79905ac49e400
Cerasela-Nicola Soare <ceraselanicola@yahoo.com>
2025-11-26 11:56:11 +0200

    Rename bug

--- src/BudgetBuddy.Domain/Extensions/DecimalExtensions.cs
+++ src/BudgetBuddy.Domain/Extensions/DecimalExtensions.cs
@@ -7,23 +7,14 @@ namespace BudgetBuddy.Domain.Extensions
 {
     public static class DecimalExtensions
     {
-        public static decimal SumAbs(this decimal a, decimal b)
-        {
-            return Math.Abs(a) + Math.Abs(b);
-        }
         public static decimal SumAbs(this IEnumerable<decimal> arr)
         {
             return arr.Sum(x => Math.Abs(x));
         }
-        public static decimal AverageAbs(this decimal a, decimal b)
-        {
-            return (Math.Abs(a) + Math.Abs(b)) / 2;
-        }
-        
         public static decimal AverageAbs(this IEnumerable<decimal> arr)
         {
             ArgumentNullException.ThrowIfNull(arr);
-            return arr.Average(x => Math.Abs(x));
+            return Math.Round(arr.Average(x => Math.Abs(x)),2);
         }
 
     }
